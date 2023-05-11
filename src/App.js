5c31035f9620ef0738c669da98ad28424ef22fae
Amudala Saicharan <2000081250@hexaware.com>
2023-05-11 16:22:59 +0530

    Latest fend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; 
+
+import PortComposition from './components/Portfolio/PortComposition';
+import Theme from './components/Portfolio/Theme';
+import NewData from './components/Portfolio/NewData';
+import {Routes,Route} from 'react-router-dom';
+import LandingPage from './components/Portfolio/LandingPage';
+import PortHeader from './components/Portfolio/PortHeader';
 
 function App() {
+  
+  const addPortfolioData=(expenses)=>{
+    console.log(expenses);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+      {/* <NewData onAddData={addPortfolioData}/> */}
+      {/* <Navbar/> */}
+      <Route exact path='/LandingPage' element={<LandingPage/>}></Route> 
+      <Route path='/' element={<LandingPage/>}></Route>
+      <Route path='/portfolioheader' element={<NewData onAddData={addPortfolioData}/>}></Route>
+      <Route path='/theme' component={<Theme/>}></Route>
+      <Route path='/portcomposition' element={<PortComposition/>}></Route>
+      <Route exact path='/portheader' element={<PortHeader/>}></Route>
+    </Routes>
+
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+{/*  
+      <Add/>
+      <Try/>
+      <PortComposition/>
+      <PortHeader/>
+      <Theme/> */}
